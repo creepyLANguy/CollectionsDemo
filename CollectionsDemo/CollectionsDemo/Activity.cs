bb3e574e419f4fc7f3eafed3b4c2848d68c9edef
Altamish Mahomed <gambit318@gmail.com>
2021-06-08 23:23:10 +0200

    Simplify activity solution.

--- CollectionsDemo/CollectionsDemo/Activity.cs
+++ CollectionsDemo/CollectionsDemo/Activity.cs
@@ -1,59 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 /*
- Given an array with the following input: 
+  Given an array with the following input: 
    
-   111111
-   1111111111
-   111111111
-   11
-   11111
-   11111
-   1111
-   1111111111
-   11111111
-   1
-   1111111111
-   1111
-   11111111
-   11
-   11
-   1111111
-   111
-   
-   Print the following output: 
-   
-   1111111111
-   111111111
-   11111111
-   1111111
-   111111
-   11111
-   1111
-   111
-   11
-   1
+  111111
+  1111111111
+  111111111
+  11
+  11111
+  11111
+  1111
+  1111111111
+  11111111
+  1
+  1111111111
+  1111
+  1111111111
+  11111111
+  11
+  11
+  1111111111  
+  1111111
+  111
+  
+  Print the following output: 
+  
+  1111111111
+  111111111
+  11111111
+  1111111
+  111111
+  11111
+  1111
+  111
+  11
+  1
    
   You are required to call the Output() function for displaying your final output.
 
-   Constraints: 
+  Constraints: 
    
-   - You may not explicitly check the length or value of any input row. 
-   - You may not explicitly compare any input rows.
-   - You may not modify any of the input rows.
-   - You may not populate collections by using indexes. 
-   - You must ingest all rows of the input. (Best start with the example class containing the array)
+  - You may not explicitly check the length or value of any input row. 
+  - You may not explicitly compare any input rows.
+  - You may not modify any of the input rows.
+  - You may not populate collections by using indexes. 
+  - You must ingest all rows of the input. (Best start with the example class containing the array)
    
-   Tips: 
-   - Rely on the nature of the collections of the collections we've discussed today and use them to achieve the output, 
-     without resorting to traditional methods or manual operations on the data. 
-   - Curiously, you may notice that the input has duplicate rows... 👀
+  Tips: 
+  - Rely on the nature of the collections of the collections we've discussed today and use them to achieve the output, 
+    without resorting to traditional methods or manual operations on the data.
+  - Think about transforming one collection into another, and into another... 
+  - Curiously, you may notice that the input has duplicate rows... 👀
    
-   Bonus Points: 
-   - Solve using 3 different collections and without calling Reverse() method.
+  Bonus Points: 
+  - Solve using 3 different collections and without calling Reverse() method.
  */
 
 namespace CollectionsDemo
@@ -73,9 +74,11 @@ namespace CollectionsDemo
       1,
       1111111111,
       1111,
+      1111111111,
       11111111,
       11,
       11,
+      1111111111,
       1111111,
       111
     };
@@ -84,25 +87,14 @@ namespace CollectionsDemo
     {
       Output(input);
 
-      var set = new HashSet<int>();
-      foreach (var item in input)
-      {
-        set.Add(item);
-      }
-
+      var set = new HashSet<int>(input);
       Output(set);
 
-      var list = set.ToList();
+      var list = new List<int>(set); //var list = set.ToList();
       list.Sort();
-
       Output(list);
 
-      var stack = new Stack<int>();
-      foreach (var item in list)
-      {
-        stack.Push(item);
-      }
-
+      var stack = new Stack<int>(list);
       Output(stack);
     }
   }
